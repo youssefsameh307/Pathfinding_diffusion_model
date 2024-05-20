Fixed bugs when scaling up the model. and trained bigger models

model/encoder.py
@@ -56,17 +56,27 @@ class FlattenEmbeder(Encoder):
 
     """
 
-    def __init__(self, input_sample,  embedding_dim=64,  normalizer=None,device='cuda'):
+    def __init__(self, input_sample,  embedding_dim=64,  normalizer=None,device='cuda', num_of_hidden_layers=1):
         super(FlattenEmbeder, self).__init__()
 
 
         # get the input dimension
         input_dim = input_sample.view(-1).size(0)
+        
+        # create hidden layers
+        hidden_layers = []
+        current_dim = input_dim
+        for i in range(num_of_hidden_layers-1):
+            hidden_layers.append(nn.Linear(current_dim, current_dim//2))
+            hidden_layers.append(nn.SELU())
+            current_dim = current_dim//2
 
         # flatten the input sample
         self.embedding = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(input_dim, embedding_dim),
+            # add the hidden layers
+            *hidden_layers,
+            nn.Linear(current_dim, embedding_dim),
             nn.SELU()
         )
         self.device = device

model/models.py
@@ -77,6 +77,7 @@ class TemporalUnet(nn.Module):
         num_resolutions = len(in_out)
 
         print(in_out)
+        horizon_values = [horizon]
         for ind, (dim_in, dim_out) in enumerate(in_out):
             is_last = ind >= (num_resolutions - 1)
 
@@ -89,13 +90,16 @@ class TemporalUnet(nn.Module):
 
             if not is_last:
                 horizon = horizon // 2
+                horizon_values.append(horizon)
 
         mid_dim = dims[-1]
         self.mid_block1 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=time_dim, horizon=horizon, device=device)
         self.mid_attn = Residual(PreNorm(mid_dim, LinearAttention(mid_dim))) if attention else nn.Identity()
         self.mid_block2 = ResidualTemporalBlock(mid_dim, mid_dim, embed_dim=time_dim, horizon=horizon, device=device)
 
-        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+        out_in = reversed(in_out[1:])
+        for ind, (dim_in, dim_out) in enumerate(out_in):
+            
             is_last = ind >= (num_resolutions - 1)
 
             self.ups.append(nn.ModuleList([
@@ -106,7 +110,7 @@ class TemporalUnet(nn.Module):
             ]))
 
             if not is_last:
-                horizon = horizon * 2
+                horizon = horizon_values.pop()
 
         self.final_conv = nn.Sequential(
             Conv1dBlock(dim, dim, kernel_size=5),