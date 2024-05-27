de47434302ddfeffb4257d67c9296ef9d1385ac7
karim lotfy <karim.samir.lotfy@gmil.com>
2024-05-27 10:13:14 +0000

    retrain

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,4 @@ from .models import TemporalUnet
 from .diffusion import Diffusion
 from .encoder import Encoder, WorldIndexEmbeder, FlattenEmbeder
 from .loss import WeightedLoss, ObstacleFreePathLoss, GraphBasedConsistencyLoss
-<<<<<<< HEAD
 from .vae import VAEXperiment
-=======
->>>>>>> origin/conditoning-for-start-goal-of-path
