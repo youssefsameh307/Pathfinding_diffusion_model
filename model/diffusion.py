d3bfee5f99516dc0b9b84638bcfff00f8010f8c2
karim lotfy <karim.samir.lotfy@gmil.com>
2024-06-02 09:47:51 +0000

    Added cosine sheulder

--- model/diffusion.py
+++ model/diffusion.py
@@ -19,8 +19,57 @@ class Diffusion():
         self.alpha_hat = torch.cumprod(self.alpha, dim=0)
         self.device = device
 
-    def prepare_noise_schedule(self):
-        return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
+    def prepare_noise_schedule(self, scheduler_type='liner'):
+        if scheduler_type=='liner':
+            return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
+        if scheduler_type=='cosine':
+            return self.betas_for_alpha_bar(self.noise_steps, max_beta=0.999, alpha_transform_type="cosine")
+            
+    
+    def betas_for_alpha_bar(
+        self,
+        num_diffusion_timesteps,
+        max_beta=0.999,
+        alpha_transform_type="cosine",
+    ):
+        """
+        Create a beta schedule that discretizes the given alpha_t_bar function, which defines the cumulative product of
+        (1-beta) over time from t = [0,1].
+
+        Contains a function alpha_bar that takes an argument t and transforms it to the cumulative product of (1-beta) up
+        to that part of the diffusion process.
+
+
+        Args:
+            num_diffusion_timesteps (`int`): the number of betas to produce.
+            max_beta (`float`): the maximum beta to use; use values lower than 1 to
+                        prevent singularities.
+            alpha_transform_type (`str`, *optional*, default to `cosine`): the type of noise schedule for alpha_bar.
+                        Choose from `cosine` or `exp`
+
+        Returns:
+            betas (`np.ndarray`): the betas used by the scheduler to step the model outputs
+        """
+        if alpha_transform_type == "cosine":
+
+            def alpha_bar_fn(t):
+                return np.cos((t + 0.008) / 1.008 * np.pi / 2) ** 2
+
+        elif alpha_transform_type == "exp":
+
+            def alpha_bar_fn(t):
+                return np.exp(t * -12.0)
+
+        else:
+            raise ValueError(f"Unsupported alpha_transform_type: {alpha_transform_type}")
+
+        betas = []
+        for i in range(num_diffusion_timesteps):
+            t1 = i / num_diffusion_timesteps
+            t2 = (i + 1) / num_diffusion_timesteps
+            betas.append(min(1 - alpha_bar_fn(t2) / alpha_bar_fn(t1), max_beta))
+        return torch.tensor(betas, dtype=torch.float32)
+
 
     def noise_input(self, x, t):
         sqrt_alpha = torch.sqrt(self.alpha[t])[:, None, None]
